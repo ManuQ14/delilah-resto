a713df7e5e069151850ec453a6dc9d3e1aad2588
Manu Quintana <49887331+ManuQ14@users.noreply.github.com>
2021-05-27 11:10:50 -0300

    Add files via upload

--- routes/api.router.js
+++ routes/api.router.js
@@ -6,38 +6,24 @@ const userValidation = require('../validations/users.validations');
 
 
 //User routers
-router.post('/usuarios/login', usuariosController.authenticate); //Login
-
-router.post('/usuarios', userValidation.isAuthorized, userValidation.isAdmin, usuariosController.createUsuarios); //Crear un nuevo usuario
-
-//router.get('usuarios/:id', userValidation.isAuthorized, usuariosController.getById)
-
-
+router.post('/usuarios', userValidation.isAuthorized, userValidation.isAdmin, usuariosController.createUsuarios); //Crea usuario
+router.get('/usuarios/login', usuariosController.authenticate); //Login
 
 
 //Products routers
 router.post('/productos', userValidation.isAuthorized, userValidation.isAdmin, productosController.createProductos);
-
 router.get('/productos', userValidation.isAuthorized, productosController.getProductos);
-
 router.get('/productos/:id', userValidation.isAuthorized, productosController.getById);
-
 router.put('/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, productosController.updateProductos);
-
-router.delete('/productos/:id', userValidation.isAuthorized, userValidation.isAuthorized, productosController.deleteProductos);
-
+router.delete('/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, productosController.deleteProductos);
 
 
 //Pedidos routers
 router.post('/pedidos', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.createPedidos);
-
 router.get('/pedidos', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.getPedidos);
-
-router.get('/pedidos/:id/productos/:id', userValidation.isAuthorized, pedidosController.getPedidosById);
-
-router.put('/pedidos/:id/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.updatePedidos);
-
-router.delete('/pedidos:id/productos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.deletePedidos);
+router.get('/pedidos/:id', userValidation.isAuthorized, pedidosController.getPedidosById);
+router.put('/pedidos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.updatePedidos);
+router.delete('/pedidos/:id', userValidation.isAuthorized, userValidation.isAdmin, pedidosController.deletePedidos);
 
 
 module.exports = router
